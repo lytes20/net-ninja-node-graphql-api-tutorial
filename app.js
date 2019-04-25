6ce2adb6b72362c8aeb8594d2cb366a884cd6f3e
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2019-04-25 23:58:47 +0300

    Added DB

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config()
+console.log("DB user name-->", process.env.DB_USERNAME)
+
+const connectionString =
+  "mongodb+srv://"+process.env.DB_USERNAME+":"+process.env.DB_PASSWORD+""+process.env.DB_NAME+".mongodb.net/test?retryWrites=true";
+mongoose.connect(connectionString);
+mongoose.connection.once("open", () => {
+  console.log("Connected to the database");
+});
 
 const port = process.env.PORT || 4000;
 
-app.use("/graph", graphqlHTTP({
+app.use(
+  "/graph",
+  graphqlHTTP({
     schema,
     graphiql: true
-}));
+  })
+);
 
 app.listen(port, () => {
   console.log("Yaapp ... ");
